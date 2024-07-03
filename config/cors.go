91b99d2e5e85285fe03786694ead0ea64718a787
Shahadat Hossain <axel.tahmid@gmail.com>
2024-07-03 08:28:53 +0600

    chore: update cors headers

--- config/cors.go
+++ config/cors.go
@@ -5,7 +5,7 @@ import "github.com/kelseyhightower/envconfig"
 type Cors struct {
 	AllowedOrigins   []string `split_words:"true" required:"true"`
 	AllowedMethods   []string `split_words:"true" default:"GET,POST,PUT,DELETE,PATCH,OPTIONS"`
-	AllowedHeaders   []string `split_words:"true" default:"Origin,Content-Type,Accept,Authorization"`
+	AllowedHeaders   []string `split_words:"true" default:"Origin,Authorization,User-Agent,Content-Type,Accept,Accept-Encoding,Accept-Language,Cache-Control,Connection,DNT,Host,Origin,Pragma,Referer"`
 	AllowCredentials bool     `split_words:"true" default:"true"`
 	MaxAge           int      `split_words:"true" default:"300"`
 }
