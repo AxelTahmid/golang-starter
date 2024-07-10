f7f020a5359c6425b64628d12e23a69297d0eb7e
Shahadat Hossain <axel.tahmid@gmail.com>
2024-07-10 10:38:28 +0600

    Remove unused error messages and refactor success messages

---
+++ internal/utils/message/success.go
@@ -0,0 +1,8 @@
+package message
+
+const (
+	SuccessLogin    = "user login successful"
+	SuccessRegister = "user register successful"
+	SuccessMe       = "user fetched successfully"
+	SucessRefresh   = "token refreshed successfully"
+)
